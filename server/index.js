2c5860c7547127371cbb1857670f83ff20226065
Mandar1511 <96373283+Mandar1511@users.noreply.github.com>
2024-09-14 15:18:33 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const app = express();
 const { Server } = require("socket.io");
 const cors = require("cors");
 
-app.use(cors());
+app.use(cors()); // remote
 
 const server = http.createServer(app);
 
