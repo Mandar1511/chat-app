e7d0dbe058875978df8c4791eaf43add4cd5cfe8
Mandar <mandardeshpande2003@gmail.com>
2024-09-14 15:18:44 +0530

    local

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const app = express();
 const { Server } = require("socket.io");
 const cors = require("cors");
 
-app.use(cors());
+app.use(cors()); // local
 
 const server = http.createServer(app);
 
